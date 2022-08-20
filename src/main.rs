422b6f831027d60d98eb30a535e4d52e85963c63
Ajeet Khadke <akhadke@avaya.com>
2022-08-20 13:24:13 +0530

    fixed error added file write to file feature

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-use std::io::{self, BufRead};
+use std::io::{self,Write,BufRead};
 use colored::*;
-use serde_json::{Result, Value};
+use serde_json::{Value};
 use clap::Parser;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 use chrono::{Datelike, Timelike, Utc};
 use std::fs::File;
-use std::io::{Write};
-fn print_log(data: &String) -> Result<()> {
-    let v: Value = serde_json::from_str(&data)?;
+fn print_log(data: String) -> Option<String> {
+    let success= serde_json::from_str(&data);
+    if success.is_ok(){
+      let v: Value = success.unwrap(); 
     let level = color_level(if !v["level"].is_null() {v["level"].as_str().unwrap()} else{""});
     let timestamp =  if !v["timestamp"].is_null() {v["timestamp"].as_str().unwrap()} else{""};
     let logger = if !v["logger"].is_null() {v["logger"].as_str().unwrap()} else{""};
     let message = if !v["message"].is_null() {v["message"].as_str().unwrap()} else{""};
     let stack = if !v["stack"].is_null() {v["stack"].as_str().unwrap().red()} else{"".normal()};
-    
+    let d=format!("{} {} {} {}:{} ", timestamp,level,logger,message,stack);
+    Some(d)
+    }else {
+    Some(data)
+    }
     // Access parts of the data by indexing with square brackets.
-    println!("{} {} {} {}:{} ", timestamp,level,logger,message,stack);
-    Ok(())
 }
 
 fn color_level(level:&str)->ColoredString{    
@@ -53,23 +56,22 @@ fn main() {
         //println!("File name {}",file_path.unwrap())
       }
 
-
-    let mut output:File;
+    let mut output :Option<File>;
     let stdin = io::stdin();
     if !file_path.is_none() {
-    let mut output = File::create(file_path.unwrap()).expect("Error while creating file");
+    output = Some(File::create(file_path.unwrap()).expect("Cant create file"));
+    }else{
+      output=None;
     }
     for line in stdin.lock().lines() {
-        let mut sline =line.as_ref().unwrap();
-        
-       // println!(" \x1b[1m Available capacity: \x1B[33m{}\x1b[0m",sline);
+      let sline =line.unwrap();
       let result=  print_log(sline);
-      if !result.is_ok() {
-        println!("{}", sline);
-        write!(output, sline);
-      }
-      
-         
+      if result.is_some() {
+        println!("{}", result.as_ref().unwrap());        
+      if let Some(ref mut file) = output {
+        writeln!(file,"{}", result.as_ref().unwrap()).expect("file not found");
+    } 
+    } 
     }
    
 }
